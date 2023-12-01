9f2d6862d6a2018d196a0ed8af108a2cbc7de55b
Riccardo <riccardo.mattia2002@gmail.com>
2023-12-01 12:18:40 +0100

    login activity and associated layout

---
+++ app/src/main/java/it/unimib/travelhub/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package it.unimib.travelhub.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.unimib.travelhub.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
