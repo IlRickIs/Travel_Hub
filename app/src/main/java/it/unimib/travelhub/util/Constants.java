f59cb72136987e3f3d1b5d019b4c8bafc01b313a
Paolo Cislaghi <paolocisly@gmail.com>
2024-06-07 12:54:48 +0200

    FIX Snackbar remove after pushup FIX Travels sort FIX multiple OngoingTravels

--- app/src/main/java/it/unimib/travelhub/util/Constants.java
+++ app/src/main/java/it/unimib/travelhub/util/Constants.java
@@ -31,7 +31,7 @@ public class Constants {
 
     public static final String TRAVEL_ADDED = "TRAVEL_ADDED";
 
-    public static final int TRAVELS_DATABASE_VERSION = 1;
+    public static final int TRAVELS_DATABASE_VERSION = 2;
     public static final String TRAVELS_DATABASE_NAME = "travels_database";
     public static final String LAST_UPDATE = "last_update";
 
